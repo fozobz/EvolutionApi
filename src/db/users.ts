2f7ec145d723f2bde66710c2a5f0a815e324ae93
fozobz <98126543+fozobz@users.noreply.github.com>
2025-12-27 14:16:36 -0800

    project setup

---
+++ src/db/users.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {type:String, required: true},
+    authentication:{
+        password: {type: String, required:true, select: false},
+        salt: {type: String, select: false},
+        sessionToken: {type: String, select: false},
+    },
+});
+
+export const userModel = mongoose.model('User', UserSchema);
+
+export const getUsers = () => find();
